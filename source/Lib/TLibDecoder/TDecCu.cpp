8fbaf12dee358db8c6122f883706bfcee1b95fe9
seregin <seregin@6a6dfc5a-121a-4be2-95cf-7b65356f9147>
2012-11-16 17:59:17 +0000

    Bug fix for IntraBL 4x4 transform using inverse DCT. Patch was provided by Patrice Onno <Patrice.Onno@crf.canon.fr>

--- source/Lib/TLibDecoder/TDecCu.cpp
+++ source/Lib/TLibDecoder/TDecCu.cpp
@@ -555,7 +555,18 @@ TDecCu::xIntraRecLumaBlk( TComDataCU* pcCU,
 
   Int scalingListType = (pcCU->isIntra(uiAbsPartIdx) ? 0 : 3) + g_eTTable[(Int)TEXT_LUMA];
   assert(scalingListType < 6);
+#if INTRA_BL_DCT_FIX
+  if(pcCU->isIntraBL(uiAbsPartIdx))
+  {
+    m_pcTrQuant->invtransformNxN( pcCU->getCUTransquantBypass(uiAbsPartIdx), TEXT_LUMA, REG_DCT, piResi, uiStride, pcCoeff, uiWidth, uiHeight, scalingListType, useTransformSkip );
+  }
+  else
+  {
+    m_pcTrQuant->invtransformNxN( pcCU->getCUTransquantBypass(uiAbsPartIdx), TEXT_LUMA, pcCU->getLumaIntraDir( uiAbsPartIdx ), piResi, uiStride, pcCoeff, uiWidth, uiHeight, scalingListType, useTransformSkip );
+  }
+#else
   m_pcTrQuant->invtransformNxN( pcCU->getCUTransquantBypass(uiAbsPartIdx), TEXT_LUMA, pcCU->getLumaIntraDir( uiAbsPartIdx ), piResi, uiStride, pcCoeff, uiWidth, uiHeight, scalingListType, useTransformSkip );
+#endif
 
   
   //===== reconstruction =====
