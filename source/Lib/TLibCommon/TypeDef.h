ba0925edcdfbde3a46facb8a9781246bc70d45bf
seregin <seregin@6a6dfc5a-121a-4be2-95cf-7b65356f9147>
2013-05-02 23:35:32 +0000

    typo correction

--- source/Lib/TLibCommon/TypeDef.h
+++ source/Lib/TLibCommon/TypeDef.h
@@ -62,9 +62,8 @@
 #define CHROMA_UPSAMPLING                1      ///< L0335: Chroma upsampling with 5 bits coefficients
 
 #define SIMPLIFIED_MV_POS_SCALING        1      ///< M0133/M0449: inter-layer MV scaling and pixel mapping position calculation
-#define ILP_DECODED_PICTURE              1      ///< M0274: use decoded picture size for inter-layer prediction
+#define ILP_DECODED_PICTURE              1      ///< M0274: use decoded picture for inter-layer prediction
 #define JCTVC_M0259_LAMBDAREFINEMENT     1      ///< JCTVC-M0259: lambda refinement (encoder only optimization)
-
 
 #define AVC_BASE                         0      ///< YUV BL reading for AVC base SVC
 #define REF_IDX_FRAMEWORK                1      ///< inter-layer reference framework
