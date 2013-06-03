b559e0707179d2873d26fa9eafc3df6a528efc02
seregin <seregin@6a6dfc5a-121a-4be2-95cf-7b65356f9147>
2013-06-03 18:02:26 +0000

    create a branch for multilayer implementation

---
+++ branches/SHM-2.1-multilayers-dev/source/App/TAppEncoder/TAppEncLayerCfg.h
@@ -0,0 +1,124 @@
+
+/** \file     TAppEncLayerCfg.h
+    \brief    Handle encoder layer configuration parameters (header)
+*/
+#ifndef __TAPPENCLAYERCFG__
+#define __TAPPENCLAYERCFG__
+
+#include "TLibCommon/CommonDef.h"
+#include "TLibEncoder/TEncCfg.h"
+#include <sstream>
+
+using namespace std;
+#if SVC_EXTENSION
+class TAppEncCfg;
+#endif
+//! \ingroup TAppEncoder
+//! \{
+
+// ====================================================================================================================
+// Class definition
+// ====================================================================================================================
+
+/// encoder layer configuration class
+class TAppEncLayerCfg
+{
+  friend class TAppEncCfg;
+  friend class TAppEncTop;
+protected:
+  // file I/O0
+  string    m_cInputFile;                                     ///< source file name
+  string    m_cReconFile;                                     ///< output reconstruction file
+
+  Int       m_iFrameRate;                                     ///< source frame-rates (Hz)
+  Int       m_iSourceWidth;                                   ///< source width in pixel
+  Int       m_iSourceHeight;                                  ///< source height in pixel
+  Int       m_conformanceMode;
+  Int       m_confLeft;
+  Int       m_confRight;
+  Int       m_confTop;
+  Int       m_confBottom;
+  Int       m_aiPad[2];                                       ///< number of padded pixels for width and height
+  Int       m_iIntraPeriod;                                   ///< period of I-slice (random access period)
+  Double    m_fQP;                                            ///< QP value of key-picture (floating point)
+#if VPS_EXTN_DIRECT_REF_LAYERS
+  Int       *m_refLayerIds;
+  Int       m_numDirectRefLayers;
+#endif
+
+#if RC_SHVC_HARMONIZATION
+  Bool      m_RCEnableRateControl;                ///< enable rate control or not
+  Int       m_RCTargetBitrate;                    ///< target bitrate when rate control is enabled
+  Bool      m_RCKeepHierarchicalBit;              ///< whether keeping hierarchical bit allocation structure or not
+  Bool      m_RCLCULevelRC;                       ///< true: LCU level rate control; false: picture level rate control
+  Bool      m_RCUseLCUSeparateModel;              ///< use separate R-lambda model at LCU level
+  Int       m_RCInitialQP;                        ///< inital QP for rate control
+  Bool      m_RCForceIntraQP;                     ///< force all intra picture to use initial QP or not
+#endif
+
+#if SVC_EXTENSION
+  Int       m_iWaveFrontSubstreams; //< If iWaveFrontSynchro, this is the number of substreams per frame (dependent tiles) or per tile (independent tiles).
+#endif
+
+  Int       m_iQP;                                            ///< QP value of key-picture (integer)
+  char*     m_pchdQPFile;                                     ///< QP offset for each slice (initialized from external file)
+  Int*      m_aidQP;                                          ///< array of slice QP values
+  TAppEncCfg* m_cAppEncCfg;                                   ///< pointer to app encoder config
+#if SCALED_REF_LAYER_OFFSETS
+  Int       m_scaledRefLayerLeftOffset;
+  Int       m_scaledRefLayerTopOffset;
+  Int       m_scaledRefLayerRightOffset;
+  Int       m_scaledRefLayerBottomOffset;
+#endif  
+public:
+  TAppEncLayerCfg();
+  virtual ~TAppEncLayerCfg();
+
+public:
+  Void  create    ();                                         ///< create option handling class
+  Void  destroy   ();                                         ///< destroy option handling class
+  bool  parseCfg  ( const string& cfgFileName );              ///< parse layer configuration file to fill member variables
+
+#if AVC_SYNTAX
+  Void  xPrintParameter( UInt layerId );
+#else
+  Void  xPrintParameter();
+#endif
+  Bool  xCheckParameter();
+
+  Void    setAppEncCfg(TAppEncCfg* p) {m_cAppEncCfg = p;          }
+
+  string  getInputFile()              {return m_cInputFile;       }
+  string  getReconFile()              {return m_cReconFile;       }
+  Int     getFrameRate()              {return m_iFrameRate;       }
+  Int     getSourceWidth()            {return m_iSourceWidth;     }
+  Int     getSourceHeight()           {return m_iSourceHeight;    }
+  Int     getConformanceMode()        { return m_conformanceMode; }
+  Int*    getPad()                    {return m_aiPad;            }
+  Double  getFloatQP()                {return m_fQP;              }
+  Int     getConfLeft()               {return m_confLeft;         }
+  Int     getConfRight()              {return m_confRight;        }
+  Int     getConfTop()                {return m_confTop;          }
+  Int     getConfBottom()             {return m_confBottom;       }
+
+  Int     getIntQP()                  {return m_iQP;              } 
+  Int*    getdQPs()                   {return m_aidQP;            }
+#if VPS_EXTN_DIRECT_REF_LAYERS
+  Int     getNumDirectRefLayers()     {return m_numDirectRefLayers;}
+  Int*    getRefLayerIds()            {return m_refLayerIds;      }
+  Int     getRefLayerId(Int i)        {return m_refLayerIds[i];   }
+#endif
+#if RC_SHVC_HARMONIZATION
+  Bool    getRCEnableRateControl()    {return m_RCEnableRateControl;   }
+  Int     getRCTargetBitrate()        {return m_RCTargetBitrate;       }
+  Bool    getRCKeepHierarchicalBit()  {return m_RCKeepHierarchicalBit; }
+  Bool    getRCLCULevelRC()           {return m_RCLCULevelRC;          }
+  Bool    getRCUseLCUSeparateModel()  {return m_RCUseLCUSeparateModel; }
+  Int     getRCInitialQP()            {return m_RCInitialQP;           }
+  Bool    getRCForceIntraQP()         {return m_RCForceIntraQP;        }
+#endif
+}; // END CLASS DEFINITION TAppEncLayerCfg
+
+//! \}
+
+#endif // __TAPPENCLAYERCFG__
---
+++ branches/SHM-2.1-multilayers-dev/source/Lib/TLibDecoder/TDecTop.h
@@ -0,0 +1,199 @@
+/* The copyright in this software is being made available under the BSD
+ * License, included below. This software may be subject to other third party
+ * and contributor rights, including patent rights, and no such rights are
+ * granted under this license.  
+ *
+ * Copyright (c) 2010-2013, ITU/ISO/IEC
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *  * Neither the name of the ITU/ISO/IEC nor the names of its contributors may
+ *    be used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/** \file     TDecTop.h
+    \brief    decoder class (header)
+*/
+
+#ifndef __TDECTOP__
+#define __TDECTOP__
+
+#include "TLibCommon/CommonDef.h"
+#include "TLibCommon/TComList.h"
+#include "TLibCommon/TComPicYuv.h"
+#include "TLibCommon/TComPic.h"
+#include "TLibCommon/TComTrQuant.h"
+#include "TLibCommon/SEI.h"
+
+#include "TDecGop.h"
+#include "TDecEntropy.h"
+#include "TDecSbac.h"
+#include "TDecCAVLC.h"
+#include "SEIread.h"
+
+struct InputNALUnit;
+
+//! \ingroup TLibDecoder
+//! \{
+
+// ====================================================================================================================
+// Class definition
+// ====================================================================================================================
+
+/// decoder class
+class TDecTop
+{
+private:
+  Int                     m_iMaxRefPicNum;
+  
+  Int                     m_pocCRA;            ///< POC number of the latest CRA picture
+  Bool                    m_prevRAPisBLA;      ///< true if the previous RAP (CRA/CRANT/BLA/BLANT/IDR) picture is a BLA/BLANT picture
+  Int                     m_pocRandomAccess;   ///< POC number of the random access point (the first IDR or CRA picture)
+
+  TComList<TComPic*>      m_cListPic;         //  Dynamic buffer
+#if SVC_EXTENSION
+  ParameterSetManagerDecoder m_parameterSetManagerDecoder[MAX_LAYERS];  // storage for parameter sets 
+#else
+  ParameterSetManagerDecoder m_parameterSetManagerDecoder;  // storage for parameter sets 
+#endif
+
+  TComSlice*              m_apcSlicePilot;
+  
+  SEIMessages             m_SEIs; ///< List of SEI messages that have been received before the first slice and between slices
+
+  // functional classes
+  TComPrediction          m_cPrediction;
+  TComTrQuant             m_cTrQuant;
+  TDecGop                 m_cGopDecoder;
+  TDecSlice               m_cSliceDecoder;
+  TDecCu                  m_cCuDecoder;
+  TDecEntropy             m_cEntropyDecoder;
+  TDecCavlc               m_cCavlcDecoder;
+  TDecSbac                m_cSbacDecoder;
+  TDecBinCABAC            m_cBinCABAC;
+  SEIReader               m_seiReader;
+  TComLoopFilter          m_cLoopFilter;
+  TComSampleAdaptiveOffset m_cSAO;
+
+  Bool isSkipPictureForBLA(Int& iPOCLastDisplay);
+  Bool isRandomAccessSkipPicture(Int& iSkipFrame,  Int& iPOCLastDisplay);
+  TComPic*                m_pcPic;
+  UInt                    m_uiSliceIdx;
+#if !SVC_EXTENSION
+  Int                     m_prevPOC;
+#endif
+  Bool                    m_bFirstSliceInPicture;
+#if !SVC_EXTENSION
+  Bool                    m_bFirstSliceInSequence;
+#endif
+
+#if SVC_EXTENSION
+  static UInt             m_prevPOC;        // POC of the previous slice
+  static UInt             m_uiPrevLayerId;  // LayerId of the previous slice
+  static Bool             m_bFirstSliceInSequence;
+  UInt                    m_layerId;      
+  UInt                    m_numLayer;
+  TDecTop**               m_ppcTDecTop;
+#if AVC_BASE
+  fstream*                m_pBLReconFile;
+  Int                     m_iBLSourceWidth;
+  Int                     m_iBLSourceHeight;
+#endif
+#endif 
+#if AVC_SYNTAX || SYNTAX_OUTPUT
+  fstream*               m_pBLSyntaxFile;
+#endif
+#if REF_IDX_FRAMEWORK
+  TComPic*                m_cIlpPic[MAX_NUM_REF];                    ///<  Inter layer Prediction picture =  upsampled picture 
+#endif
+
+public:
+  TDecTop();
+  virtual ~TDecTop();
+  
+  Void  create  ();
+  Void  destroy ();
+
+  void setDecodedPictureHashSEIEnabled(Int enabled) { m_cGopDecoder.setDecodedPictureHashSEIEnabled(enabled); }
+
+  Void  init();
+#if SVC_EXTENSION
+  Bool  decode(InputNALUnit& nalu, Int& iSkipFrame, Int& iPOCLastDisplay, UInt& curLayerId, Bool& bNewPOC);
+#else
+  Bool  decode(InputNALUnit& nalu, Int& iSkipFrame, Int& iPOCLastDisplay);
+#endif
+  
+  Void  deletePicBuffer();
+
+  Void executeLoopFilters(Int& poc, TComList<TComPic*>*& rpcListPic);
+
+#if SVC_EXTENSION
+  UInt      getLayerId            () { return m_layerId;              }
+  Void      setLayerId            (UInt layer) { m_layerId = layer; }
+  UInt      getNumLayer           () { return m_numLayer;             }
+  Void      setNumLayer           (UInt uiNum)   { m_numLayer = uiNum;  }
+  TComList<TComPic*>*      getListPic() { return &m_cListPic; }
+  Void      setLayerDec(TDecTop **p)    { m_ppcTDecTop = p; }
+  TDecTop*  getLayerDec(UInt layer)     { return m_ppcTDecTop[layer]; }
+#if VPS_EXTN_DIRECT_REF_LAYERS
+  TDecTop*  getRefLayerDec(UInt refLayerIdc);
+#endif
+#if AVC_BASE
+  Void      setBLReconFile( fstream* pFile ) { m_pBLReconFile = pFile; }
+  fstream*  getBLReconFile() { return m_pBLReconFile; }
+  Void      setBLsize( Int iWidth, Int iHeight ) { m_iBLSourceWidth = iWidth; m_iBLSourceHeight = iHeight; }
+  Int       getBLWidth() { return  m_iBLSourceWidth; }
+  Int       getBLHeight() { return  m_iBLSourceHeight; }
+#endif
+#endif
+#if AVC_SYNTAX || SYNTAX_OUTPUT
+  Void      setBLSyntaxFile( fstream* pFile ) { m_pBLSyntaxFile = pFile; }
+  fstream* getBLSyntaxFile() { return m_pBLSyntaxFile; }
+#endif
+#if REF_IDX_FRAMEWORK
+  Void      xInitILRP(TComSPS *pcSPS);
+  Void      setILRPic(TComPic *pcPic);
+#endif
+
+protected:
+  Void  xGetNewPicBuffer  (TComSlice* pcSlice, TComPic*& rpcPic);
+  Void  xCreateLostPicture (Int iLostPOC);
+
+  Void      xActivateParameterSets();
+#if SVC_EXTENSION
+  Bool      xDecodeSlice(InputNALUnit &nalu, Int &iSkipFrame, Int iPOCLastDisplay, UInt& curLayerId, Bool& bNewPOC);
+#else
+  Bool      xDecodeSlice(InputNALUnit &nalu, Int &iSkipFrame, Int iPOCLastDisplay);
+#endif
+  Void      xDecodeVPS();
+  Void      xDecodeSPS();
+  Void      xDecodePPS();
+  Void      xDecodeSEI( TComInputBitstream* bs, const NalUnitType nalUnitType );
+
+};// END CLASS DEFINITION TDecTop
+
+
+//! \}
+
+#endif // __TDECTOP__
+
