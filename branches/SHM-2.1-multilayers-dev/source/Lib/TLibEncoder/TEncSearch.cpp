1f03e8ca438df5354eca5b1c95fecabbfd73299e
seregin <seregin@6a6dfc5a-121a-4be2-95cf-7b65356f9147>
2013-06-05 22:26:16 +0000

    another fix a fast encoder mode

--- branches/SHM-2.1-multilayers-dev/source/Lib/TLibEncoder/TEncSearch.cpp
+++ branches/SHM-2.1-multilayers-dev/source/Lib/TLibEncoder/TEncSearch.cpp
@@ -3312,11 +3312,7 @@ Void TEncSearch::predInterSearch( TComDataCU* pcCU, TComYuv* pcOrgYuv, TComYuv*&
       RefPicList  eRefPicList = ( iRefList ? REF_PIC_LIST_1 : REF_PIC_LIST_0 );
       
 #if (ENCODER_FAST_MODE)
-#if JCTVC_M0458_INTERLAYER_RPS_SIG
-      if( pcCU->getSlice()->getNumRefIdx(eRefPicList) - pcCU->getSlice()->getActiveNumILRRefIdx() > 0 )
-#else
-      if( pcCU->getSlice()->getNumRefIdx(eRefPicList) - pcCU->getSlice()->getVPS()->getNumDirectRefLayers( pcCU->getLayerId() ) > 0 )
-#endif
+      if( pcCU->getSlice()->getNumRefIdx(eRefPicList) > 1 )
       {
         testILR = false;
       }
