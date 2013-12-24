f77d13d072ea7ef7e633a41cd32f7bb6bb7f3571
qualcomm <qualcomm@6a6dfc5a-121a-4be2-95cf-7b65356f9147>
2013-12-24 02:04:21 +0000

    Using signaled DPB-related parameters in the decoder (MACRO: USE_DPB_SIZE_TABLE)
    This patch uses the signaled value of DPB-related parameters in the VPS extension in the decoder.
    
    From: Adarsh K. Ramasubramonian <aramasub@qti.qualcomm.com>

--- branches/SHM-4.1-dev/source/Lib/TLibDecoder/TDecTop.cpp
+++ branches/SHM-4.1-dev/source/Lib/TLibDecoder/TDecTop.cpp
@@ -169,7 +169,13 @@ Void TDecTop::xInitILRP(TComSlice *slice)
 
     for( Int temporalLayer=0; temporalLayer < MAX_TLAYER; temporalLayer++) 
     {
+#if USE_DPB_SIZE_TABLE
+      TComVPS *vps = slice->getVPS();
+      // SHM decoders will use DPB size table in the VPS to determine the number of reorder pictures.
+      numReorderPics[temporalLayer] = vps->getMaxVpsNumReorderPics( getCommonDecoderParams()->getOutputLayerSetIdx() , temporalLayer);
+#else
       numReorderPics[temporalLayer] = pcSPS->getNumReorderPics(temporalLayer);
+#endif
     }
 
     if (m_cIlpPic[0] == NULL)
@@ -259,10 +265,20 @@ Void TDecTop::xGetNewPicBuffer ( TComSlice* pcSlice, TComPic*& rpcPic )
 
   for( Int temporalLayer=0; temporalLayer < MAX_TLAYER; temporalLayer++) 
   {
+#if USE_DPB_SIZE_TABLE
+    TComVPS *vps = pcSlice->getVPS();
+    // SHM decoders will use DPB size table in the VPS to determine the number of reorder pictures.
+    numReorderPics[temporalLayer] = vps->getMaxVpsNumReorderPics( getCommonDecoderParams()->getOutputLayerSetIdx() , temporalLayer);
+#else
     numReorderPics[temporalLayer] = pcSlice->getSPS()->getNumReorderPics(temporalLayer);
+#endif
   }
 
+#if USE_DPB_SIZE_TABLE
+  m_iMaxRefPicNum = pcSlice->getVPS()->getMaxVpsDecPicBufferingMinus1( getCommonDecoderParams()->getOutputLayerSetIdx(), pcSlice->getLayerId(), pcSlice->getTLayer() ) + 1; // m_uiMaxDecPicBuffering has the space for the picture currently being decoded
+#else
   m_iMaxRefPicNum = pcSlice->getSPS()->getMaxDecPicBuffering(pcSlice->getTLayer());     // m_uiMaxDecPicBuffering has the space for the picture currently being decoded
+#endif
 
 #if SVC_EXTENSION
   m_iMaxRefPicNum += 1; // it should be updated if more than 1 resampling picture is used
