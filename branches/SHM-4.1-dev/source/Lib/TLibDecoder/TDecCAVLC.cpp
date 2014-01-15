71971fc57d0295efeb3101939757b4e84e4d9889
qualcomm <qualcomm@6a6dfc5a-121a-4be2-95cf-7b65356f9147>
2014-01-15 00:06:24 +0000

    JCTVC-O0137: Reserve value 63 for vps_max_layers_minus1 (Macro: O0137_MAX_LAYERID)
    Reserve the value of 63 for vps_max_layers_minus1, but allow bistreams to contain the value. Decoder ignore NUTs with layer ID equal to 63. 
    
    Patch from Karsten Suehring <karsten.suehring@hhi.fraunhofer.de>
    
    From: Adarsh K. Ramasubramonian <aramasub@qti.qualcomm.com>

--- branches/SHM-4.1-dev/source/Lib/TLibDecoder/TDecCAVLC.cpp
+++ branches/SHM-4.1-dev/source/Lib/TLibDecoder/TDecCAVLC.cpp
@@ -865,7 +865,11 @@ Void TDecCavlc::parseVPS(TComVPS* pcVPS)
   READ_CODE( 4,  uiCode,  "vps_video_parameter_set_id" );         pcVPS->setVPSId( uiCode );
   READ_CODE( 2,  uiCode,  "vps_reserved_three_2bits" );           assert(uiCode == 3);
 #if VPS_RENAME
-  READ_CODE( 6,  uiCode,  "vps_max_layers_minus1" );              pcVPS->setMaxLayers( uiCode + 1);
+#if O0137_MAX_LAYERID
+  READ_CODE( 6,  uiCode,  "vps_max_layers_minus1" );              pcVPS->setMaxLayers( min( 62u, uiCode) + 1 );
+#else
+  READ_CODE( 6,  uiCode,  "vps_max_layers_minus1" );              pcVPS->setMaxLayers( uiCode + 1 );
+#endif
 #else
   READ_CODE( 6,  uiCode,  "vps_reserved_zero_6bits" );            assert(uiCode == 0);
 #endif
