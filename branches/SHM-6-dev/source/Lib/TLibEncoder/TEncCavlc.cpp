251c19645d65a50bf626659a0a43072595650266
seregin <seregin@6a6dfc5a-121a-4be2-95cf-7b65356f9147>
2014-04-08 21:20:05 +0000

    fix signaling of base_layer_parameter_set_compatibility_flag

--- branches/SHM-6-dev/source/Lib/TLibEncoder/TEncCavlc.cpp
+++ branches/SHM-6-dev/source/Lib/TLibEncoder/TEncCavlc.cpp
@@ -1563,17 +1563,17 @@ Void TEncCavlc::codeVPSVUI (TComVPS *vps)
 #if P0182_VPS_VUI_PS_FLAG
     for(i = 1; i < vps->getMaxLayers(); i++)
     {
-      if(vps->getNumRefLayers(vps->getLayerIdInNuh(i)) == 0) 
+      if( vps->getNumRefLayers(vps->getLayerIdInNuh(i)) == 0 ) 
       {
-        if ((vps->getSPSId(i) == 0) && (vps->getPPSId(i) == 0))
+        if( (vps->getSPSId(i) == 0) && (vps->getPPSId(i) == 0) )
         {
           vps->setBaseLayerPSCompatibilityFlag(i, 1);
-          WRITE_FLAG(vps->getBaseLayerPSCompatibilityFlag(i), "base_layer_parameter_set_compatibility_flag" );
         }
         else
         {
           vps->setBaseLayerPSCompatibilityFlag(i, 0);
         }
+        WRITE_FLAG(vps->getBaseLayerPSCompatibilityFlag(i), "base_layer_parameter_set_compatibility_flag" );
       }
     }
 #endif
