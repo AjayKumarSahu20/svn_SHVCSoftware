b94205854f48dd5ef8b321f79ccd97fc0e147b48
seregin <seregin@6a6dfc5a-121a-4be2-95cf-7b65356f9147>
2014-04-21 02:45:33 +0000

    create a tag for SHM-6.0

---
+++ tags/SHM-6.0/source/Lib/TLibCommon/TCom3DAsymLUT.cpp
@@ -0,0 +1,279 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <algorithm>
+
+#include "TypeDef.h"
+#include "TCom3DAsymLUT.h"
+#include "TComPicYuv.h"
+
+#if Q0048_CGS_3D_ASYMLUT
+
+const Int TCom3DAsymLUT::m_nVertexIdxOffset[4][3] = { { 0 , 0 , 0 } , { 0 , 1 , 0 } , { 0 , 1 , 1 } , { 1 , 1 , 1 } };
+
+TCom3DAsymLUT::TCom3DAsymLUT()
+{
+  m_pCuboid = NULL;
+  m_nResQuanBit = 0;
+}
+
+TCom3DAsymLUT::~TCom3DAsymLUT()
+{
+  destroy();
+}
+
+Void TCom3DAsymLUT::create( Int nMaxOctantDepth , Int nInputBitDepth , Int nInputBitDepthC , Int nOutputBitDepth , Int nOutputBitDepthC , Int nMaxYPartNumLog2 )
+{
+  m_nMaxOctantDepth = nMaxOctantDepth;
+  m_nInputBitDepthY = nInputBitDepth;
+  m_nOutputBitDepthY = nOutputBitDepth;
+  m_nInputBitDepthC = nInputBitDepthC;
+  m_nOutputBitDepthC = nOutputBitDepthC;
+  m_nDeltaBitDepthC = m_nOutputBitDepthC - m_nInputBitDepthC;
+  m_nDeltaBitDepth = m_nOutputBitDepthY - m_nInputBitDepthY;
+  m_nMaxYPartNumLog2 = nMaxYPartNumLog2;
+  m_nMaxPartNumLog2 = 3 * m_nMaxOctantDepth + m_nMaxYPartNumLog2;
+
+  xUpdatePartitioning( nMaxOctantDepth , nMaxYPartNumLog2 );
+
+  m_nYSize = 1 << ( m_nMaxOctantDepth + m_nMaxYPartNumLog2 );
+  m_nUSize = 1 << m_nMaxOctantDepth;
+  m_nVSize = 1 << m_nMaxOctantDepth;
+  assert( m_nYSize > 0 && m_nUSize > 0 && m_nVSize > 0 );
+
+  if( m_pCuboid != NULL )
+    destroy();
+  xAllocate3DArray( m_pCuboid , m_nYSize , m_nUSize , m_nVSize );
+}
+
+Void TCom3DAsymLUT::destroy()
+{
+  xFree3DArray( m_pCuboid );
+}
+
+Void TCom3DAsymLUT::xUpdatePartitioning( Int nCurOctantDepth , Int nCurYPartNumLog2 )
+{
+  assert( nCurOctantDepth <= m_nMaxOctantDepth );
+  assert( nCurYPartNumLog2 <= m_nMaxYPartNumLog2 );
+
+  m_nCurOctantDepth = nCurOctantDepth;
+  m_nCurYPartNumLog2 = nCurYPartNumLog2;
+  m_nYShift2Idx = m_nInputBitDepthY - m_nCurOctantDepth - m_nCurYPartNumLog2;
+  m_nUShift2Idx = m_nVShift2Idx = m_nInputBitDepthC - m_nCurOctantDepth;
+  m_nMappingShift = m_nYShift2Idx + m_nUShift2Idx;
+  m_nMappingOffset = 1 << ( m_nMappingShift - 1 );
+}
+
+Void TCom3DAsymLUT::colorMapping( TComPicYuv * pcPic, TComPicYuv * pcPicDst )
+{
+  Int nWidth = pcPic->getWidth();
+  Int nHeight = pcPic->getHeight();
+  Int nStrideY = pcPic->getStride();
+  Int nStrideC = pcPic->getCStride();
+  Pel * pY = pcPic->getLumaAddr();
+  Pel * pU = pcPic->getCbAddr();
+  Pel * pV = pcPic->getCrAddr();
+
+  Int nDstStrideY = pcPicDst->getStride();
+  Int nDstStrideC = pcPicDst->getCStride();
+  Pel * pYDst = pcPicDst->getLumaAddr();
+  Pel * pUDst = pcPicDst->getCbAddr();
+  Pel * pVDst = pcPicDst->getCrAddr();
+
+  Pel *pUPrev = pU;
+  Pel *pVPrev = pV;
+  Pel *pUNext = pU+nStrideC;
+  Pel *pVNext = pV+nStrideC;
+
+  // alignment padding
+  pU += (nWidth>>1);
+  pV += (nWidth>>1);
+  for( Int y = 0 ; y < (nHeight>>1) ; y ++ )
+  {
+    *pU = pU[-1];
+    *pV = pV[-1];
+    pU += nStrideC;
+    pV += nStrideC;
+  }
+  memcpy(pU-(nWidth>>1), pU-(nWidth>>1)-nStrideC, ((nWidth>>1)+1)*sizeof(Pel));
+  memcpy(pV-(nWidth>>1), pV-(nWidth>>1)-nStrideC, ((nWidth>>1)+1)*sizeof(Pel));
+  pU = pcPic->getCbAddr();
+  pV = pcPic->getCrAddr();
+
+  Pel iMaxValY = (1<<getOutputBitDepthY())-1;
+  Pel iMaxValC = (1<<getOutputBitDepthC())-1;
+  for( Int y = 0 ; y < nHeight ; y += 2 )
+  {
+    for( Int xY = 0 , xC = 0 ; xY < nWidth ; xY += 2 , xC++ )
+    {
+      Pel srcY00 = pY[xY];
+      Pel srcY01 = pY[xY+1];
+      Pel srcY10 = pY[xY+nStrideY];
+      Pel srcY11 = pY[xY+nStrideY+1];
+      Pel srcYaver;
+      Pel srcU = pU[xC];
+      Pel srcV = pV[xC];
+      Pel dstY00, dstY01, dstY10, dstY11;
+
+      // alignment
+      srcYaver =  (srcY00 + srcY10 + 1 ) >> 1;
+      Pel srcUP0 = pUPrev[xC];
+      Pel srcVP0 = pVPrev[xC];        
+      Pel tmpU =  (srcUP0 + srcU + (srcU<<1) + 2 ) >> 2;
+      Pel tmpV =  (srcVP0 + srcV + (srcV<<1) + 2 ) >> 2;
+      dstY00 = xMapY( srcY00 , tmpU , tmpV );
+      Pel a = pU[xC+1] + srcU;
+      tmpU =  ((a<<1) + a + srcUP0 + pUPrev[xC+1] + 4 ) >> 3;
+      Pel b = pV[xC+1] + srcV;
+      tmpV =  ((b<<1) + b + srcVP0 + pVPrev[xC+1] + 4 ) >> 3;
+      dstY01 = xMapY( srcY01 , tmpU , tmpV );
+
+      srcUP0 = pUNext[xC];
+      srcVP0 = pVNext[xC];
+      tmpU =  (srcUP0 + srcU + (srcU<<1) + 2 ) >> 2;
+      tmpV =  (srcVP0 + srcV + (srcV<<1) + 2 ) >> 2;
+      dstY10 = xMapY( srcY10 , tmpU , tmpV );
+      tmpU =  ((a<<1) + a + srcUP0 + pUNext[xC+1] + 4 ) >> 3;
+      tmpV =  ((b<<1) + b + srcVP0 + pVNext[xC+1] + 4 ) >> 3;
+      dstY11 = xMapY( srcY11 , tmpU , tmpV );
+
+      SYUVP dstUV = xMapUV( srcYaver , srcU , srcV );
+      pYDst[xY] = Clip3((Pel)0, iMaxValY, dstY00 );
+      pYDst[xY+1] = Clip3((Pel)0, iMaxValY, dstY01 );
+      pYDst[xY+nDstStrideY] = Clip3((Pel)0, iMaxValY, dstY10 );
+      pYDst[xY+nDstStrideY+1] = Clip3((Pel)0, iMaxValY, dstY11 );
+      pUDst[xC] = Clip3((Pel)0, iMaxValC, dstUV.U );
+      pVDst[xC] = Clip3((Pel)0, iMaxValC, dstUV.V );
+    }
+    pY += nStrideY + nStrideY;
+
+    // alignment
+    pUPrev = pU;
+    pVPrev = pV;
+    pU = pUNext;
+    pV = pVNext;
+    pUNext += nStrideC;
+    pVNext += nStrideC;
+
+    pYDst += nDstStrideY + nDstStrideY;
+    pUDst += nDstStrideC;
+    pVDst += nDstStrideC;
+  }
+}
+
+SYUVP TCom3DAsymLUT::xGetCuboidVertexPredA( Int yIdx , Int uIdx , Int vIdx , Int nVertexIdx )
+{
+  assert( nVertexIdx < 4 );
+  
+  SYUVP sPred;
+  sPred.Y = ( yIdx + m_nVertexIdxOffset[nVertexIdx][0] ) << ( m_nYShift2Idx + m_nDeltaBitDepth );
+  sPred.U = ( uIdx + m_nVertexIdxOffset[nVertexIdx][1] ) << ( m_nUShift2Idx + m_nDeltaBitDepthC );
+  sPred.V = ( vIdx + m_nVertexIdxOffset[nVertexIdx][2] ) << ( m_nVShift2Idx + m_nDeltaBitDepthC );
+  return( sPred );
+}
+
+SYUVP  TCom3DAsymLUT::xGetCuboidVertexPredAll( Int yIdx , Int uIdx , Int vIdx , Int nVertexIdx , SCuboid *** pCurCuboid )
+{
+  SCuboid***  pCuboid = pCurCuboid ? pCurCuboid : m_pCuboid ;
+
+  // PredA
+  SYUVP sPredA = xGetCuboidVertexPredA( yIdx , uIdx , vIdx , nVertexIdx );
+
+  // PredB
+  SYUVP sPredB; 
+  memset( &sPredB , 0 , sizeof( sPredB ) );
+  if( yIdx > 0 )
+  {
+    SYUVP & recNeighborP = pCuboid[yIdx-1][uIdx][vIdx].P[nVertexIdx];
+    SYUVP sPredNeighbor = xGetCuboidVertexPredA( yIdx - 1 , uIdx , vIdx , nVertexIdx );
+    sPredB.Y += recNeighborP.Y - sPredNeighbor.Y ;
+    sPredB.U += recNeighborP.U - sPredNeighbor.U ;
+    sPredB.V += recNeighborP.V - sPredNeighbor.V ;
+
+    Pel min = - ( 1 << ( getOutputBitDepthY() - 2 ) );
+    Pel max =  - min;
+    sPredB.Y = Clip3( min , max , sPredB.Y );
+    min = - ( 1 << ( getOutputBitDepthC() - 2 ) );
+    max =  - min;
+    sPredB.U = Clip3( min , max , sPredB.U );
+    sPredB.V = Clip3( min , max , sPredB.V );
+  }
+
+  SYUVP sPred;
+  sPred.Y = sPredA.Y + sPredB.Y;
+  sPred.U = sPredA.U + sPredB.U;
+  sPred.V = sPredA.V + sPredB.V;
+
+  return sPred ;
+}
+
+SYUVP TCom3DAsymLUT::getCuboidVertexResTree( Int yIdx , Int uIdx , Int vIdx , Int nVertexIdx )
+{
+  const SYUVP & rYUVP = m_pCuboid[yIdx][uIdx][vIdx].P[nVertexIdx];
+  SYUVP sPred = xGetCuboidVertexPredAll( yIdx , uIdx , vIdx , nVertexIdx );
+
+  SYUVP sResidue;
+  sResidue.Y = ( rYUVP.Y - sPred.Y ) >> m_nResQuanBit;
+  sResidue.U = ( rYUVP.U - sPred.U ) >> m_nResQuanBit;
+  sResidue.V = ( rYUVP.V - sPred.V ) >> m_nResQuanBit;
+  return( sResidue );
+}
+
+Void TCom3DAsymLUT::setCuboidVertexResTree( Int yIdx , Int uIdx , Int vIdx , Int nVertexIdx , Int deltaY , Int deltaU , Int deltaV )
+{
+  SYUVP & rYUVP = m_pCuboid[yIdx][uIdx][vIdx].P[nVertexIdx];
+  SYUVP sPred = xGetCuboidVertexPredAll( yIdx , uIdx , vIdx , nVertexIdx );
+
+  rYUVP.Y = sPred.Y + ( deltaY << m_nResQuanBit );
+  rYUVP.U = sPred.U + ( deltaU << m_nResQuanBit );
+  rYUVP.V = sPred.V + ( deltaV << m_nResQuanBit );
+}
+
+Pel TCom3DAsymLUT::xMapY( Pel y , Pel u , Pel v )
+{
+  const SCuboid & rCuboid = m_pCuboid[y>>m_nYShift2Idx][u>>m_nUShift2Idx][v>>m_nVShift2Idx];
+  Pel dstY = rCuboid.P[0].Y;
+  Int deltaY = y - ( y >> m_nYShift2Idx << m_nYShift2Idx );
+  Int deltaU = u - ( u >> m_nUShift2Idx << m_nUShift2Idx );
+  Int deltaV = v - ( v >> m_nVShift2Idx << m_nVShift2Idx );
+  dstY += ( Pel )( ( ( ( deltaY * ( rCuboid.P[3].Y - rCuboid.P[2].Y ) ) << m_nUShift2Idx ) 
+                   + ( ( deltaU * ( rCuboid.P[1].Y - rCuboid.P[0].Y ) ) << m_nYShift2Idx )
+                   + ( ( deltaV * ( rCuboid.P[2].Y - rCuboid.P[1].Y ) ) << m_nYShift2Idx ) 
+                   + m_nMappingOffset ) >> m_nMappingShift );
+  return( dstY );
+}
+
+SYUVP TCom3DAsymLUT::xMapUV( Pel y , Pel u , Pel v )
+{
+  const SCuboid & rCuboid = m_pCuboid[y>>m_nYShift2Idx][u>>m_nUShift2Idx][v>>m_nVShift2Idx];
+  SYUVP dst = rCuboid.P[0];
+  Int deltaY = y - ( y >> m_nYShift2Idx << m_nYShift2Idx );
+  Int deltaU = u - ( u >> m_nUShift2Idx << m_nUShift2Idx );
+  Int deltaV = v - ( v >> m_nVShift2Idx << m_nVShift2Idx );
+  dst.U += ( Pel )( ( ( ( deltaY * ( rCuboid.P[3].U - rCuboid.P[2].U ) ) << m_nUShift2Idx ) 
+                    + ( ( deltaU * ( rCuboid.P[1].U - rCuboid.P[0].U ) ) << m_nYShift2Idx )
+                    + ( ( deltaV * ( rCuboid.P[2].U - rCuboid.P[1].U ) ) << m_nYShift2Idx ) 
+                    + m_nMappingOffset ) >> m_nMappingShift );
+  dst.V += ( Pel )( ( ( ( deltaY * ( rCuboid.P[3].V - rCuboid.P[2].V ) ) << m_nUShift2Idx ) 
+                    + ( ( deltaU * ( rCuboid.P[1].V - rCuboid.P[0].V ) ) << m_nYShift2Idx )
+                    + ( ( deltaV * ( rCuboid.P[2].V - rCuboid.P[1].V ) ) << m_nYShift2Idx ) 
+                    + m_nMappingOffset ) >> m_nMappingShift );
+  return( dst );
+}
+
+Void TCom3DAsymLUT::xSaveCuboids( SCuboid *** pSrcCuboid )
+{
+  memcpy( m_pCuboid[0][0] , pSrcCuboid[0][0] , sizeof( SCuboid ) * m_nYSize * m_nUSize * m_nVSize );
+}
+
+Void TCom3DAsymLUT::copy3DAsymLUT( TCom3DAsymLUT * pSrc )
+{
+  assert( pSrc->getMaxOctantDepth() == getMaxOctantDepth() && pSrc->getMaxYPartNumLog2() == getMaxYPartNumLog2() );
+  xUpdatePartitioning( pSrc->getCurOctantDepth() , pSrc->getCurYPartNumLog2() );
+  setResQuantBit( pSrc->getResQuantBit() );
+  xSaveCuboids( pSrc->m_pCuboid );
+}
+
+#endif
+
---
+++ tags/SHM-6.0/source/Lib/TLibDecoder/TDecTop.h
@@ -0,0 +1,354 @@
+/* The copyright in this software is being made available under the BSD
+ * License, included below. This software may be subject to other third party
+ * and contributor rights, including patent rights, and no such rights are
+ * granted under this license.  
+ *
+ * Copyright (c) 2010-2014, ITU/ISO/IEC
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *  * Neither the name of the ITU/ISO/IEC nor the names of its contributors may
+ *    be used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/** \file     TDecTop.h
+    \brief    decoder class (header)
+*/
+
+#ifndef __TDECTOP__
+#define __TDECTOP__
+
+#include "TLibCommon/CommonDef.h"
+#include "TLibCommon/TComList.h"
+#include "TLibCommon/TComPicYuv.h"
+#include "TLibCommon/TComPic.h"
+#include "TLibCommon/TComTrQuant.h"
+#include "TLibCommon/SEI.h"
+#if Q0048_CGS_3D_ASYMLUT
+#include "TLibCommon/TCom3DAsymLUT.h"
+#endif
+
+#include "TDecGop.h"
+#include "TDecEntropy.h"
+#include "TDecSbac.h"
+#include "TDecCAVLC.h"
+#include "SEIread.h"
+
+struct InputNALUnit;
+
+//! \ingroup TLibDecoder
+//! \{
+
+// ====================================================================================================================
+// Class definition
+// ====================================================================================================================
+
+#if Q0074_SEI_COLOR_MAPPING
+class TDecColorMapping
+{
+  Int   m_colorMapId;
+  Bool  m_colorMapCancelFlag;
+  Bool  m_colorMapPersistenceFlag;
+  Bool  m_colorMap_video_signal_type_present_flag;
+  Bool  m_colorMap_video_full_range_flag;
+  Int   m_colorMap_primaries;
+  Int   m_colorMap_transfer_characteristics;
+  Int   m_colorMap_matrix_coeffs;
+  Int   m_colorMapModelId;
+
+  Int   m_colour_map_coded_data_bit_depth;
+  Int   m_colour_map_target_bit_depth;
+
+  Int   m_num_input_pivots[3];
+  Int*  m_coded_input_pivot_value[3];
+  Int*  m_target_input_pivot_value[3];
+  
+  Bool  m_matrix_flag;
+  Int   m_log2_matrix_denom;
+  Int   m_matrix_coef[3][3];
+
+  Int   m_num_output_pivots[3];
+  Int*  m_coded_output_pivot_value[3];
+  Int*  m_target_output_pivot_value[3];
+
+  Bool  m_lut1d_computed[3];
+  Int*  m_lut1d_input[3];
+  Int*  m_lut1d_output[3];
+  TComPicYuv* m_pcColorMappingPic[2];
+
+public:
+  TDecColorMapping();
+  ~TDecColorMapping();
+
+  Bool        getColorMappingFlag()                     { return(!m_colorMapCancelFlag);};
+
+  Void        setColorMapping( SEIMessages m_SEIs );
+  Void        setColorMapping( Int bitDepthY, Int bitDepthC );
+  TComPicYuv* getColorMapping( TComPicYuv* pPicYuvRec, Int iTop=0, Int curlayerId=0 );
+};// END CLASS DEFINITION TDecColorMapping
+#endif
+
+/// decoder class
+class TDecTop
+{
+private:
+  Int                     m_iMaxRefPicNum;
+  
+  NalUnitType             m_associatedIRAPType; ///< NAL unit type of the associated IRAP picture
+  Int                     m_pocCRA;            ///< POC number of the latest CRA picture
+  Int                     m_pocRandomAccess;   ///< POC number of the random access point (the first IDR or CRA picture)
+
+  TComList<TComPic*>      m_cListPic;         //  Dynamic buffer
+  ParameterSetManagerDecoder m_parameterSetManagerDecoder;  // storage for parameter sets 
+  TComSlice*              m_apcSlicePilot;
+
+  SEIMessages             m_SEIs; ///< List of SEI messages that have been received before the first slice and between slices
+
+  // functional classes
+  TComPrediction          m_cPrediction;
+#if Q0048_CGS_3D_ASYMLUT
+  TCom3DAsymLUT           m_c3DAsymLUTPPS;
+  TComPicYuv*             m_pColorMappedPic;
+#endif
+  TComTrQuant             m_cTrQuant;
+  TDecGop                 m_cGopDecoder;
+  TDecSlice               m_cSliceDecoder;
+  TDecCu                  m_cCuDecoder;
+  TDecEntropy             m_cEntropyDecoder;
+  TDecCavlc               m_cCavlcDecoder;
+  TDecSbac                m_cSbacDecoder;
+  TDecBinCABAC            m_cBinCABAC;
+  SEIReader               m_seiReader;
+  TComLoopFilter          m_cLoopFilter;
+  TComSampleAdaptiveOffset m_cSAO;
+
+  Bool isSkipPictureForBLA(Int& iPOCLastDisplay);
+  Bool isRandomAccessSkipPicture(Int& iSkipFrame,  Int& iPOCLastDisplay);
+  TComPic*                m_pcPic;
+  UInt                    m_uiSliceIdx;
+#if !SVC_EXTENSION
+  Int                     m_prevPOC;
+#endif
+  Bool                    m_bFirstSliceInPicture;
+#if !SVC_EXTENSION
+  Bool                    m_bFirstSliceInSequence;
+#endif
+  Bool                    m_prevSliceSkipped;
+  Int                     m_skippedPOC;
+#if SETTING_NO_OUT_PIC_PRIOR  
+  Bool                    m_bFirstSliceInBitstream;
+  Int                     m_lastPOCNoOutputPriorPics;
+  Bool                    m_isNoOutputPriorPics;
+  Bool                    m_craNoRaslOutputFlag;    //value of variable NoRaslOutputFlag of the last CRA pic
+#endif
+
+#if SVC_EXTENSION
+  static UInt             m_prevPOC;        // POC of the previous slice
+  static UInt             m_uiPrevLayerId;  // LayerId of the previous slice
+  static Bool             m_bFirstSliceInSequence;
+  UInt                    m_layerId;      
+  UInt                    m_numLayer;
+  TDecTop**               m_ppcTDecTop;
+#if AVC_BASE
+  fstream*                m_pBLReconFile;
+#if !REPN_FORMAT_IN_VPS
+  Int                     m_iBLSourceWidth;
+  Int                     m_iBLSourceHeight;
+#endif
+#endif
+#if VPS_EXTN_DIRECT_REF_LAYERS
+  Int                     m_numDirectRefLayers;
+  Int                     m_refLayerId[MAX_VPS_LAYER_ID_PLUS1];
+  Int                     m_numSamplePredRefLayers;
+  Int                     m_samplePredRefLayerId[MAX_VPS_LAYER_ID_PLUS1];
+  Int                     m_numMotionPredRefLayers;
+  Int                     m_motionPredRefLayerId[MAX_VPS_LAYER_ID_PLUS1];
+  Bool                    m_samplePredEnabledFlag[MAX_VPS_LAYER_ID_PLUS1];
+  Bool                    m_motionPredEnabledFlag[MAX_VPS_LAYER_ID_PLUS1];
+#endif
+  TComPic*                m_cIlpPic[MAX_NUM_REF];                    ///<  Inter layer Prediction picture =  upsampled picture
+#endif 
+#if OUTPUT_LAYER_SET_INDEX
+  CommonDecoderParams*    m_commonDecoderParams;
+#endif
+#if AVC_SYNTAX || SYNTAX_OUTPUT
+  fstream*               m_pBLSyntaxFile;
+#endif
+
+#if NO_CLRAS_OUTPUT_FLAG  
+  Bool                    m_noClrasOutputFlag;
+  Bool                    m_layerInitializedFlag;
+  Bool                    m_firstPicInLayerDecodedFlag;
+#endif
+#if RESOLUTION_BASED_DPB
+  Int                     m_subDpbIdx;     // Index to the sub-DPB that the layer belongs to.
+                                           // When new VPS is activated, this should be re-initialized to -1
+#endif
+public:
+#if Q0074_SEI_COLOR_MAPPING
+  TDecColorMapping* m_ColorMapping;
+#endif
+
+  TDecTop();
+  virtual ~TDecTop();
+  
+  Void  create  ();
+  Void  destroy ();
+
+  void setDecodedPictureHashSEIEnabled(Int enabled) { m_cGopDecoder.setDecodedPictureHashSEIEnabled(enabled); }
+
+  Void  init();
+#if SVC_EXTENSION
+  Bool  decode(InputNALUnit& nalu, Int& iSkipFrame, Int& iPOCLastDisplay, UInt& curLayerId, Bool& bNewPOC);
+#else
+  Bool  decode(InputNALUnit& nalu, Int& iSkipFrame, Int& iPOCLastDisplay);
+#endif
+  
+  Void  deletePicBuffer();
+
+  
+  TComSPS* getActiveSPS() { return m_parameterSetManagerDecoder.getActiveSPS(); }
+
+
+  Void executeLoopFilters(Int& poc, TComList<TComPic*>*& rpcListPic);
+#if SETTING_NO_OUT_PIC_PRIOR  
+  Void  checkNoOutputPriorPics (TComList<TComPic*>*& rpcListPic);
+  Bool  getNoOutputPriorPicsFlag ()         { return m_isNoOutputPriorPics; }
+  Void  setNoOutputPriorPicsFlag (Bool val) { m_isNoOutputPriorPics = val; }
+#endif
+
+#if SVC_EXTENSION
+#if EARLY_REF_PIC_MARKING
+  Void earlyPicMarking(Int maxTemporalLayer, std::vector<Int>& targetDecLayerIdList);
+#endif
+  UInt      getLayerId            () { return m_layerId;              }
+  Void      setLayerId            (UInt layer) { m_layerId = layer; }
+  UInt      getNumLayer           () { return m_numLayer;             }
+  Void      setNumLayer           (UInt uiNum)   { m_numLayer = uiNum;  }
+  TComList<TComPic*>*      getListPic() { return &m_cListPic; }
+  Void      setLayerDec(TDecTop **p)    { m_ppcTDecTop = p; }
+  TDecTop*  getLayerDec(UInt layer)     { return m_ppcTDecTop[layer]; }
+#if VPS_EXTN_DIRECT_REF_LAYERS
+  TDecTop*  getRefLayerDec(UInt refLayerIdc);
+  Int       getNumDirectRefLayers           ()                              { return m_numDirectRefLayers;      }
+  Void      setNumDirectRefLayers           (Int num)                       { m_numDirectRefLayers = num;       }
+
+  Int       getRefLayerId                   (Int i)                         { return m_refLayerId[i];           }
+  Void      setRefLayerId                   (Int i, Int refLayerId)         { m_refLayerId[i] = refLayerId;     }
+
+  Int       getNumSamplePredRefLayers       ()                              { return m_numSamplePredRefLayers;  }
+  Void      setNumSamplePredRefLayers       (Int num)                       { m_numSamplePredRefLayers = num;   }
+
+  Int       getSamplePredRefLayerId         (Int i)                         { return m_samplePredRefLayerId[i];       }
+  Void      setSamplePredRefLayerId         (Int i, Int refLayerId)         { m_samplePredRefLayerId[i] = refLayerId; }
+
+  Int       getNumMotionPredRefLayers       ()                              { return m_numMotionPredRefLayers;  }
+  Void      setNumMotionPredRefLayers       (Int num)                       { m_numMotionPredRefLayers = num;   }
+
+  Int       getMotionPredRefLayerId         (Int i)                         { return m_motionPredRefLayerId[i];       }
+  Void      setMotionPredRefLayerId         (Int i, Int refLayerId)         { m_motionPredRefLayerId[i] = refLayerId; }
+
+  Bool      getSamplePredEnabledFlag        (Int i)                         { return m_samplePredEnabledFlag[i];  }
+  Void      setSamplePredEnabledFlag        (Int i,Bool flag)               { m_samplePredEnabledFlag[i] = flag;  }
+
+  Bool      getMotionPredEnabledFlag        (Int i)                         { return m_motionPredEnabledFlag[i];  }
+  Void      setMotionPredEnabledFlag        (Int i,Bool flag)               { m_motionPredEnabledFlag[i] = flag;  }
+
+  TDecTop*  getSamplePredRefLayerDec        ( UInt layerId );
+  TDecTop*  getMotionPredRefLayerDec        ( UInt layerId );
+
+  Void      setRefLayerParams( TComVPS* vps );
+#endif
+#if AVC_BASE
+  Void      setBLReconFile( fstream* pFile ) { m_pBLReconFile = pFile; }
+  fstream*  getBLReconFile() { return m_pBLReconFile; }
+#if !REPN_FORMAT_IN_VPS
+  Void      setBLsize( Int iWidth, Int iHeight ) { m_iBLSourceWidth = iWidth; m_iBLSourceHeight = iHeight; }
+  Int       getBLWidth() { return  m_iBLSourceWidth; }
+  Int       getBLHeight() { return  m_iBLSourceHeight; }
+#endif
+#endif
+#if REPN_FORMAT_IN_VPS
+  Void      xInitILRP(TComSlice *slice);
+#else
+  Void      xInitILRP(TComSPS *pcSPS);
+#endif
+#if OUTPUT_LAYER_SET_INDEX
+  CommonDecoderParams*    getCommonDecoderParams() { return m_commonDecoderParams; }
+  Void                    setCommonDecoderParams(CommonDecoderParams* x) { m_commonDecoderParams = x; }
+  Void      checkValueOfTargetOutputLayerSetIdx(TComVPS *vps);
+#endif
+#if SCALINGLIST_INFERRING
+  ParameterSetManagerDecoder* getParameterSetManager() { return &m_parameterSetManagerDecoder; }
+#endif
+#if RESOLUTION_BASED_DPB
+  Void setSubDpbIdx(Int idx)    { m_subDpbIdx = idx; }
+  Int  getSubDpbIdx()           { return m_subDpbIdx; }
+  Void assignSubDpbs(TComVPS *vps);
+#endif
+#endif //SVC_EXTENSION
+#if AVC_SYNTAX || SYNTAX_OUTPUT
+  Void      setBLSyntaxFile( fstream* pFile ) { m_pBLSyntaxFile = pFile; }
+  fstream* getBLSyntaxFile() { return m_pBLSyntaxFile; }
+#endif
+protected:
+  Void  xGetNewPicBuffer  (TComSlice* pcSlice, TComPic*& rpcPic);
+  Void  xCreateLostPicture (Int iLostPOC);
+
+  Void      xActivateParameterSets();
+#if SVC_EXTENSION
+#if POC_RESET_FLAG
+  Bool      xDecodeSlice(InputNALUnit &nalu, Int &iSkipFrame, Int &iPOCLastDisplay, UInt& curLayerId, Bool& bNewPOC);
+#else
+  Bool      xDecodeSlice(InputNALUnit &nalu, Int &iSkipFrame, Int iPOCLastDisplay, UInt& curLayerId, Bool& bNewPOC);
+#endif
+#else
+  Bool      xDecodeSlice(InputNALUnit &nalu, Int &iSkipFrame, Int iPOCLastDisplay);
+#endif
+  Void      xDecodeVPS();
+  Void      xDecodeSPS();
+  Void      xDecodePPS(
+#if Q0048_CGS_3D_ASYMLUT
+    TCom3DAsymLUT * pc3DAsymLUT
+#endif
+    );
+  Void      xDecodeSEI( TComInputBitstream* bs, const NalUnitType nalUnitType );
+
+#if NO_CLRAS_OUTPUT_FLAG
+  Int  getNoClrasOutputFlag()                { return m_noClrasOutputFlag;}
+  Void setNoClrasOutputFlag(Bool x)          { m_noClrasOutputFlag = x;   }
+  Int  getLayerInitializedFlag()             { return m_layerInitializedFlag;}
+  Void setLayerInitializedFlag(Bool x)       { m_layerInitializedFlag = x;   }
+  Int  getFirstPicInLayerDecodedFlag()       { return m_firstPicInLayerDecodedFlag;}
+  Void setFirstPicInLayerDecodedFlag(Bool x) { m_firstPicInLayerDecodedFlag = x;   }
+#endif
+#if Q0048_CGS_3D_ASYMLUT
+  Void initAsymLut(TComSlice *pcSlice);
+#endif
+};// END CLASS DEFINITION TDecTop
+
+
+//! \}
+
+#endif // __TDECTOP__
+
