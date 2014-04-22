d537354e705c4b9a7e18996544090cfc615ffefa
seregin <seregin@6a6dfc5a-121a-4be2-95cf-7b65356f9147>
2014-04-22 17:22:47 +0000

    add a check for motion prediction type of ILRP used as a collocated picture

--- branches/SHM-6-dev/source/Lib/TLibCommon/TComPrediction.cpp
+++ branches/SHM-6-dev/source/Lib/TLibCommon/TComPrediction.cpp
@@ -487,14 +487,19 @@ Void TComPrediction::xPredInterUni ( TComDataCU* pcCU, UInt uiPartAddr, Int iWid
   pcCU->clipMv(cMv);
 
 #if SVC_EXTENSION
-  TComPic* refPic = pcCU->getSlice()->getRefPic(eRefPicList, iRefIdx);
+  if( pcCU->getLayerId() > 0 )
+  {
+    TComPic* refPic = pcCU->getSlice()->getRefPic(eRefPicList, iRefIdx);
 
-  // ILRP has to be for the sample prediction type
-  assert( ( refPic->isILR(pcCU->getLayerId()) && pcCU->getSlice()->getVPS()->isSamplePredictionType( pcCU->getLayerId(), refPic->getLayerId() ) ) || refPic->isILR(pcCU->getLayerId()) == false );
+    // It is a requirement of bitstream conformance that when the reference picture represented by the variable refIdxLX is an inter-layer reference picture, 
+    // VpsInterLayerSamplePredictionEnabled[ LayerIdxInVps[ currLayerId ] ][ LayerIdxInVps[ rLId ] ] shall be equal to 1, where rLId is set equal to nuh_layer_id of the inter-layer picture
+    assert( ( refPic->isILR(pcCU->getLayerId()) && pcCU->getSlice()->getVPS()->isSamplePredictionType( pcCU->getLayerId(), refPic->getLayerId() ) ) || refPic->isILR(pcCU->getLayerId()) == false );
 
 #if REF_IDX_ME_ZEROMV
-  assert( ( refPic->isILR(pcCU->getLayerId()) && cMv.getHor() == 0 && cMv.getVer() == 0 ) || refPic->isILR(pcCU->getLayerId()) == false );
+    // It is a requirement of bitstream conformance that the variables mvLX[ 0 ] and mvLX[ 1 ] shall be equal to 0 if the value of refIdxLX corresponds to an inter-layer reference picture. 
+    assert( ( refPic->isILR(pcCU->getLayerId()) && cMv.getHor() == 0 && cMv.getVer() == 0 ) || refPic->isILR(pcCU->getLayerId()) == false );
 #endif
+  }
 #endif
 
   xPredInterLumaBlk  ( pcCU, pcCU->getSlice()->getRefPic( eRefPicList, iRefIdx )->getPicYuvRec(), uiPartAddr, &cMv, iWidth, iHeight, rpcYuvPred, bi );
