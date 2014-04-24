c2551d90d8e75db9fc608cfe26eea77f11a0133d
qualcomm <qualcomm@6a6dfc5a-121a-4be2-95cf-7b65356f9147>
2014-04-24 23:59:04 +0000

    Implementation of JCTVC_Q0146 MACRO Q0146_SSH_EXT_DATA_BIT
    JCTVC-Q0146; Bug-fix -- the SSH_EXT_DATA_BIT can have any value -- not required to be 1
    
    Submitted by Hendry (fhendry@qti.qualcomm.com)

--- branches/SHM-6-dev/source/Lib/TLibCommon/TypeDef.h
+++ branches/SHM-6-dev/source/Lib/TLibCommon/TypeDef.h
@@ -50,6 +50,7 @@
 
 #define Q0177_SPS_TEMP_NESTING_FIX       1      ///< JCTVC-Q0177; Fix the inference value of sps_temporal_id_nesting_flag when it is not present
 #define Q0142_POC_LSB_NOT_PRESENT        1      ///< JCTVC-Q0142; Add constraint checking on the value of poc_reset_idc and poc_lsb_val
+#define Q0146_SSH_EXT_DATA_BIT           1      ///< JCTVC-Q0146; Bug-fix -- the SSH_EXT_DATA_BIT can have any value -- not required to be 1
 
 #define Q0200_CONFORMANCE_BL_SIZE        1       ///< JCTVC-Q0200; use conformance picture size in re-sampling processs
 #define P0312_VERT_PHASE_ADJ             1      ///< JCTVC-P0312: vertical phase adjustment in re-sampling process (BoG report)
--- branches/SHM-6-dev/source/Lib/TLibDecoder/TDecCAVLC.cpp
+++ branches/SHM-6-dev/source/Lib/TLibDecoder/TDecCAVLC.cpp
@@ -3267,7 +3267,11 @@ Void TDecCavlc::parseSliceHeader (TComSlice*& rpcSlice, ParameterSetManagerDecod
 
     while( counter )
     {
+#if Q0146_SSH_EXT_DATA_BIT 
+      READ_FLAG( uiCode, "slice_segment_header_extension_data_bit" );
+#else
       READ_FLAG( uiCode, "slice_segment_header_extension_reserved_bit" ); assert( uiCode == 1 );
+#endif
       counter--;
     }
   }
