7c2e86abb3fc6604f59f3183fb11a01e369a4f6b
seregin <seregin@6a6dfc5a-121a-4be2-95cf-7b65356f9147>
2014-07-25 19:36:00 +0000

    print scalability ratio

--- source/Lib/TLibDecoder/TDecGop.cpp
+++ source/Lib/TLibDecoder/TDecGop.cpp
@@ -222,7 +222,7 @@ Void TDecGop::filterPicture(TComPic*& rpcPic)
         assert( g_posScalingFactor[refLayerIdc][0] );
         assert( g_posScalingFactor[refLayerIdc][1] );
 
-        printf( "%d(%d, \{%1.2f, %1.2f\}x)", pcSlice->getRefPOC(RefPicList(iRefList), iRefIndex), refLayerId, 65536.0/g_posScalingFactor[refLayerIdc][0], 65536.0/g_posScalingFactor[refLayerIdc][1] );
+        printf( "%d(%d, {%1.2f, %1.2f}x)", pcSlice->getRefPOC(RefPicList(iRefList), iRefIndex), refLayerId, 65536.0/g_posScalingFactor[refLayerIdc][0], 65536.0/g_posScalingFactor[refLayerIdc][1] );
       }
       else
       {
