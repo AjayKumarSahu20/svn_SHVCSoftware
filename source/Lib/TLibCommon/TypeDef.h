d587c85c955dc20b6d8721adcbd99f244ba444ee
seregin <seregin@6a6dfc5a-121a-4be2-95cf-7b65356f9147>
2015-07-08 17:37:47 +0000

    macro cleanup: MAX_ONE_RESAMPLING_DIRECT_LAYERS

--- source/Lib/TLibCommon/TypeDef.h
+++ source/Lib/TLibCommon/TypeDef.h
@@ -89,7 +89,6 @@
 #define AVC_BASE                         1      ///< YUV BL reading for AVC base SVC
 
 #define REF_IDX_MFM                      1      ///< JCTVC-L0336: motion vector mapping of inter-layer reference picture
-#define MAX_ONE_RESAMPLING_DIRECT_LAYERS 1      ///< Allow maximum of one resampling process for direct reference layers
 #define MOTION_RESAMPLING_CONSTRAINT     1      ///< JCTVC-N0108: Allow maximum of one motion resampling process for direct reference layers, and use motion inter-layer prediction from the same layer as texture inter-layer prediction.
 #define AUXILIARY_PICTURES               1      ///< JCTVC-O0041: auxiliary picture layers
 #define R0062_AUX_PSEUDO_MONOCHROME      1      ///> JCVVC-R0063: pseudo monochrome for auxiliary pictures
